475fa41ecdde8ad1f1431976ff5c8f4c79020c30
nyirobalazs <balasz1995@gmail.com>
2024-03-26 14:55:51 +0000

    vesrion 0

--- src/components/InputEmptyWrapper/InputEmptyWrapper.jsx
+++ src/components/InputEmptyWrapper/InputEmptyWrapper.jsx
@@ -52,3 +52,28 @@ InputEmptyWrapper.propTypes = {
   textInputContent: PropTypes.string,
   textInputLabel: PropTypes.string,
 };
+
+class InputEmptyWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { textInputContent: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ textInputContent: event.target.value });
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.state.textInputContent}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
+
+export default InputEmptyWrapper;
